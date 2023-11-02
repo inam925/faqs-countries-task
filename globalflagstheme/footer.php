811cdc4b88abc57ccf89ed9100e0e7b9988688e1
dcen <inamulhaq925@gmail.com>
2023-11-02 10:29:43 +0500

    Updated theme

--- globalflagstheme/footer.php
+++ globalflagstheme/footer.php
@@ -1,35 +1,45 @@
 	<footer>
 		<div class="gft-footer-field">
-		<p class="gft-label">&copy;2023</p>
-		<p class="gft-value">Made by Inam Ul Haq</p>
+			<p class="gft-label">&copy;2023</p>
+			<p class="gft-value">Made by Inam Ul Haq</p>
 		</div>
-		<?php if (get_field('gft_address')) : ?>
-			<?php
-			$field = get_field_object('gft_address');
-			?>
-			<div class="gft-footer-field">
-				<p class="gft-label"><?php echo $field['label']; ?></p>
-				<p class="gft-value"><?php echo $field['value']; ?></p>
-			</div>
-		<?php endif; ?>
-		<?php if (get_field('gft_contact')) : ?>
-			<?php
-			$field = get_field_object('gft_contact');
-			?>
-			<div class="gft-footer-field">
-				<p class="gft-label"><?php echo $field['label']; ?></p>
-				<p class="gft-value"><?php echo $field['value']; ?></p>
-			</div>
-		<?php endif; ?>
-		<?php if (get_field('gft_site_logo')) : ?>
-			<?php
-			$field = get_field_object('gft_site_logo');
-			?>
-			<div class="gft-footer-field">
-				<p class="gft-label"><?php echo $field['label']; ?></p>
-				<img src="<?php echo $field['value']; ?>" class="gft-value">
-			</div>
-		<?php endif; ?>
+		<?php
+		if (in_array('advanced-custom-fields/acf.php', apply_filters('active_plugins', get_option('active_plugins')), true)) {
+		?>
+			<?php if (get_field('gft_address')) : ?>
+				<?php
+				$field = get_field_object('gft_address');
+				?>
+				<div class="gft-footer-field">
+					<p class="gft-label"><?php echo esc_html($field['label']); ?></p>
+					<p class="gft-value"><?php echo esc_html($field['value']); ?></p>
+				</div>
+			<?php endif; ?>
+			<?php if (get_field('gft_contact')) : ?>
+				<?php
+				$field = get_field_object('gft_contact');
+				?>
+				<div class="gft-footer-field">
+					<p class="gft-label"><?php echo esc_html($field['label']); ?></p>
+					<p class="gft-value"><?php echo esc_html($field['value']); ?></p>
+				</div>
+			<?php endif; ?>
+			<?php if (get_field('gft_site_logo')) : ?>
+				<?php
+				$field = get_field_object('gft_site_logo');
+				?>
+				<div class="gft-footer-field">
+					<p class="gft-label"><?php echo esc_html($field['label']); ?></p>
+					<img src="<?php echo esc_html($field['value']); ?>" class="gft-value">
+				</div>
+			<?php endif; ?>
+		<?php
+		} else {
+		?>
+			<div><?php echo esc_html__('The Advanced Custom Fields plugin is not active. Please activate it for this functionality.', 'gft_textdomain')?> </div>
+		<?php
+		}
+		?>
 	</footer>
 	<?php wp_footer(); ?>
 	</body>
--- globalflagstheme/page.php
+++ globalflagstheme/page.php
@@ -1,44 +1,54 @@
 <?php get_header(); ?>
 
-	<section id="main">
-		<h1 id="title"><?php the_title(); ?></h1>
-		<div>
-		<?php 
-		if ( is_page()) {
-			if ( have_posts() ) :
-				while ( have_posts() ) :
-					the_post(); 
-					?>
+<section id="main">
+	<h1 id="title"><?php the_title(); ?></h1>
+	<div>
+		<?php
+		if (is_page()) {
+			if (have_posts()) :
+				while (have_posts()) :
+					the_post();
+		?>
 					<article id="content">
 						<?php
 						the_content();
 						?>
 						<h1><?php the_title(); ?></h1>
-						<?php if ( get_field('gft_countries' ) ) : ?>
-						<h2>
-							<?php 
-							echo get_field('gft_countries'); 
+						<?php
+						if (in_array('advanced-custom-fields/acf.php', apply_filters('active_plugins', get_option('active_plugins')), true)) {
+						?>
+							<?php if (get_field('gft_countries')) : ?>
+								<h2>
+									<?php
+									echo get_field('gft_countries');
+									?>
+								</h2>
+
+							<?php
+							endif;
+						} else {
 							?>
-						</h2>
-						<?php 
-						endif; 
+							<div><?php echo esc_html__('The Advanced Custom Fields plugin is not active. Please activate it for this functionality.', 'gft_textdomain')?> </div>
+						<?php
+						}
 						wp_link_pages();
 						?>
 					</article>
-					<?php 
+		<?php
 				endwhile;
-		endif; } 
+			endif;
+		}
 		?>
-		</div>
+	</div>
+</section>
+
+<aside id="meta">
+	<section>
+		<h4 id="date"> <?php echo get_the_date('l - F, j Y'); ?> </h4>
 	</section>
+</aside>
 
-	<aside id="meta">
-		<section>
-			<h4 id="date"> <?php echo get_the_date('l - F, j Y'); ?> </h4>
-		</section>
-	</aside>
-
-	<hr><br>
-	<a href="/">&lt;&lt; Home Page</a>
+<hr><br>
+<a href="/">&lt;&lt; Home Page</a>
 
 <?php get_footer(); ?>
